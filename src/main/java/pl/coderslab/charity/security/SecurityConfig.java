af5f1170b8232ad5107df7807731fd94e90793d8
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2020-07-07 20:59:34 +0200

    Add SecurityClass and bean BCrypt 7_07_2020

--- src/main/java/pl/coderslab/charity/security/SecurityConfig.java
+++ src/main/java/pl/coderslab/charity/security/SecurityConfig.java
@@ -1,16 +1,22 @@
 package pl.coderslab.charity.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
