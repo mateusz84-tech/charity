23242ef61b0d563e90f032cda38850c8e5297b6e
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2020-07-07 20:56:39 +0200

    Add dependency security to pom.xml 7_07_2020

---
+++ src/main/java/pl/coderslab/charity/security/SecurityConfig.java
@@ -0,0 +1,19 @@
+package pl.coderslab.charity.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}user1").roles("USER");
+    }
+}
