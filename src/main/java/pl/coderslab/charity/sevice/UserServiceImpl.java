11dbcb0f9a80a34ac8f1e1e241e88bcb0e2dd71c
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2020-07-08 21:08:34 +0200

    8_07_2020

--- src/main/java/pl/coderslab/charity/sevice/UserServiceImpl.java
+++ src/main/java/pl/coderslab/charity/sevice/UserServiceImpl.java
@@ -2,13 +2,11 @@ package pl.coderslab.charity.sevice;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import pl.coderslab.charity.domain.model.Role;
 import pl.coderslab.charity.domain.model.User;
 import pl.coderslab.charity.domain.repository.RoleRepository;
 import pl.coderslab.charity.domain.repository.UserRepository;
-
-import java.util.Arrays;
-import java.util.HashSet;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -30,13 +28,14 @@ public class UserServiceImpl implements UserService{
         return userRepository.findByUsername(username);
     }
 
+    @Transactional
     @Override
     public void saveUser(User user) {
 
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setEnabled(1);
-        Role userRole = roleRepository.findByName("RoleUser");
-        user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+        Role userRole = roleRepository.findByName("ROLE_USER");
+        user.getRoles().add(userRole);
         userRepository.save(user);
     }
 }
